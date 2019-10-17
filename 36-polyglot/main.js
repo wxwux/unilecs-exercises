795236ff4ca2ed484b526a0c5b6e2cc6f600feba
Vladimir Sabantsev <testicola@yandex.ru>
2019-10-17 18:37:09 +0300

    переписал на цикл

--- 36-polyglot/main.js
+++ 36-polyglot/main.js
@@ -1,3 +1,7 @@
 export const countSetsCrossing = (n, sets) => {
-  return n - (n - sets[0]) - (n - sets[1]) - (n - sets[2]) - (n - sets[3]);
+  let substraction = 0;
+  for (let i = 0; i < sets.length; i++) {
+    substraction += n - sets[i];
+  }
+  return n - substraction;
 };
