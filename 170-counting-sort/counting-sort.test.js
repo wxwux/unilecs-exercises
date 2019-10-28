15dd45f78baa4024673936d5cbaaae13a4ddece8
Vladimir Sabantsev <testicola@yandex.ru>
2019-10-28 19:46:02 +0300

    тест для задачи

---
+++ 170-counting-sort/counting-sort.test.js
@@ -0,0 +1,8 @@
+import { countingSort } from "./main";
+
+test("Сортирует массив", () => {
+  const startArray = [4, 5, 2, 2, 8, 9, 8, 2, 1, 4, 1, 4, 9];
+  const resultArray = [1, 1, 2, 2, 2, 4, 4, 4, 5, 8, 8, 9, 9];
+
+  expect(countingSort(startArray)).toBe(resultArray);
+});
