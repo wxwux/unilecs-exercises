a003b34644e9d184243e981f830617a6970b03f4
Vladimir Sabantsev <testicola@yandex.ru>
2019-11-04 18:45:38 +0300

    поправил assert для сравнения

--- 170-counting-sort/counting-sort.test.js
+++ 170-counting-sort/counting-sort.test.js
@@ -4,5 +4,5 @@ test("Сортирует массив", () => {
   const startArray = [4, 5, 2, 2, 8, 9, 8, 2, 1, 4, 1, 4, 9];
   const resultArray = [1, 1, 2, 2, 2, 4, 4, 4, 5, 8, 8, 9, 9];
 
-  expect(countingSort(startArray)).toBe(resultArray);
+  expect(countingSort(startArray, 10)).toStrictEqual(resultArray);
 });
