caa30713da01731b575668dbe2615d126597ddce
Vladimir Sabantsev <testicola@yandex.ru>
2019-11-04 18:48:36 +0300

    написал сортировку подсчетом

--- 170-counting-sort/main.js
+++ 170-counting-sort/main.js
@@ -1,3 +1,14 @@
-export const countingSort = () => {
+export const countingSort = (arr, maxInt) => {
+  let countingObj = Array(maxInt).fill(0);
+  let result = [];
 
-}
+  arr.forEach(item => (countingObj[item] = ++countingObj[item]));
+
+  countingObj.forEach((times, ndx) => {
+    for (let i = 0; i < times; i++) {
+      result.push(ndx);
+    }
+  });
+
+  return result;
+};
